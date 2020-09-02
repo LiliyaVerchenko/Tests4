bd84b099ef0d73ceda51cba6110de94ee9b581ad
Liliya Verchenko <ip_verchenko@inbox.ru>
2020-09-02 22:50:28 +0300

    Added files

---
+++ test_secretery.py
@@ -0,0 +1,34 @@
+import pytest
+from data import documents, directories
+from secretery import Secretery
+secretery = Secretery('Bill', '1234567')
+
+#тест функции get_number_name
+@pytest.mark.parametrize("input,result", [('2207 876234', 'Василий Гупкин'), ('11-2', 'Геннадий Покемонов'),
+                                          ('10006', 'Аристарх Павлов'), ('102', None)]
+                        )
+def test_number_name(input, result):
+    assert secretery.get_number_name(input) == result
+
+
+#тест функции get_number_dir
+@pytest.mark.parametrize("input,result", [('2207 876234', '1'), ('11-2', '1'), ('5455 028765', '1'),
+                                          ('10006', '2'), ('12345', None)]
+                         )
+def test_number_dir(input, result):
+    assert secretery.get_number_dir(input) == result
+
+
+#тест функции get__all_list
+def test_all_list():
+    assert secretery.get__all_list() == ['passport "2207 876234" "Василий Гупкин"',
+                                         'invoice "11-2" "Геннадий Покемонов"',
+                                         'insurance "10006" "Аристарх Павлов"']
+
+#тест функции add_doc
+def test_add_doc():
+    assert secretery.add_doc('паспорт', '10', 'Милана Минаева', '2') == {'type': 'паспорт', 'number': '10', 'name': 'Милана Минаева'} == documents[-1]
+    assert '10' in directories['2']
+    assert secretery.add_doc('паспорт', '11', 'Михаил Шиндяев', '7') == None
+
+
